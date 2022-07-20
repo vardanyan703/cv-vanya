f506c1b116522df31ebf8213b5dbf4e82e278fc3
Narek Vardanyan <narekvardanyan@Nareks-iMac.local>
2022-07-20 21:11:34 +0400

    first commit

---
+++ _nuxt/static/1658337051/portfolio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcv-vanya\u002F_nuxt\u002Fstatic\u002F1658337051",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio",config:{_app:{basePath:"\u002Fcv-vanya\u002F",assetsPath:"\u002Fcv-vanya\u002F_nuxt\u002F",cdnURL:a}}}}(null));
